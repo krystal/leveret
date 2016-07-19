38f33b75aee90f09b586137385629831eab7bdbc
Dan Wentworth <dan@atechmedia.com>
2016-07-19 17:22:34 +0100

    after_deploy proc and trap INT

--- lib/leveret/configuration.rb
+++ lib/leveret/configuration.rb
@@ -1,6 +1,6 @@
 module Leveret
   class Configuration
-    attr_accessor :amqp, :exchange_name, :queue_name_prefix, :log_file, :log_level, :default_queue_name
+    attr_accessor :amqp, :exchange_name, :queue_name_prefix, :log_file, :log_level, :default_queue_name, :after_fork
 
     def initialize
       self.amqp = "amqp://guest:guest@localhost:5672/"
@@ -9,6 +9,7 @@ module Leveret
       self.log_level = Logger::DEBUG
       self.queue_name_prefix = 'leveret_queue'
       self.default_queue_name = 'standard'
+      self.after_fork = proc {}
     end
   end
 end
--- lib/leveret/worker.rb
+++ lib/leveret/worker.rb
@@ -1,34 +1,51 @@
 module Leveret
   class Worker
-    attr_accessor :queues
+    attr_accessor :queues, :consumers
 
     def initialize(*queue_names)
       queue_names = [Leveret.configuration.default_queue_name] if queue_names.empty?
+
       self.queues = queue_names.map { |name| Leveret::Queue.new(name) }
+      self.consumers = []
+      @time_to_die = false
     end
 
     def do_work
+      setup_traps
       subscribe_to_queues
-      loop do
-        sleep 10
-      end
-    end
 
-    def subscribe_to_queues
-      queues.each do |queue|
-        queue.subscribe do |msg|
-          fork_and_run(msg)
+      loop do
+        if @time_to_die
+          consumers.each(&:cancel)
+          break
         end
+        sleep 3
       end
     end
 
     private
+
+    def setup_traps
+      trap('INT') do
+        @time_to_die = true
+      end
+    end
+
+    def subscribe_to_queues
+      queues.map do |queue|
+        consumers << queue.subscribe do |msg|
+          fork_and_run(msg)
+        end
+      end
+    end
 
     def fork_and_run(msg)
       read, write = IO.pipe
 
       pid = fork do
         read.close
+
+        Leveret.configuration.after_fork.call
 
         job_klass = Object.const_get(msg['job'])
         result = job_klass.perform(msg['params'])
