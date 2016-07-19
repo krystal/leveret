1f1bc146a08b208f7b8e7f0801720cff8086487c
Dan Wentworth <dan@atechmedia.com>
2016-07-19 12:17:54 +0100

    flesh out spects for Leveret module

--- spec/leveret_spec.rb
+++ spec/leveret_spec.rb
@@ -5,17 +5,51 @@ describe Leveret do
     expect(Leveret::VERSION).not_to be nil
   end
 
-  it 'has a default configuration' do
-    expect(Leveret.configuration).to be_a(Leveret::Configuration)
+  describe '.configuration' do
+    it { expect(Leveret.configuration).to be_a(Leveret::Configuration) }
   end
 
-  it 'can be configured via block' do
-    amqp = "ampq://test-address/"
+  describe '.configure' do
+    it 'can be configured using a block' do
+      amqp = "ampq://test-address/"
 
-    Leveret.configure do |config|
-      config.amqp = amqp
+      Leveret.configure do |config|
+        config.amqp = amqp
+      end
+
+      expect(Leveret.configuration.amqp).to eq(amqp)
+    end
+  end
+
+  describe '.channel' do
+    let(:channel) { Leveret.channel }
+
+    it "must be a bunny channel" do
+      expect(channel).to be_a(Bunny::Channel)
     end
 
-    expect(Leveret.configuration.amqp).to eq(amqp)
+    it 'must prefetch only 1 message at a time' do
+      expect(channel.prefetch_count).to eq(1)
+    end
+  end
+
+  describe '.exchange' do
+    let(:exchange) { Leveret.exchange }
+
+    it 'must be a bunny exchange' do
+      expect(exchange).to be_a(Bunny::Exchange)
+    end
+
+    it 'must be named according to the configuration' do
+      expect(exchange.name).to eq(Leveret.configuration.exchange_name)
+    end
+
+    it 'must survive a rabbitmq restart' do
+      expect(exchange.durable?).to be true
+    end
+
+    it 'must survive all consumers disconnecting' do
+      expect(exchange.auto_delete?).to be false
+    end
   end
 end
