a75d5a0658b81758a0a3a1c3ab23359ba055e4db
Dan Wentworth <dan@atechmedia.com>
2016-07-22 16:56:53 +0100

    document configuration module

--- lib/leveret/configuration.rb
+++ lib/leveret/configuration.rb
@@ -1,5 +1,26 @@
 module Leveret
-  # Contains everything needed to configure leveret for work
+  # Contains everything needed to configure leveret for work. Sensible defaults are included
+  # and will be initialized with the class.
+  #
+  # @!attribute amqp
+  #   @return [String] Location of your RabbitMQ server (amqp://guest:guest@localhost:5672/)
+  # @!attribute exchange_name
+  #   @return [String] Name of the exchange for Leveret to publish messages to (leveret_exch)
+  # @!attribute queue_name_prefix
+  #   @return [String] This value will be prefixed to all queues created on your RabbitMQ instance (leveret_queue)
+  # @!attribute log_file
+  #   @return [String] The path where logfiles should be written to (STDOUT)
+  # @!attribute log_level
+  #   @return [Integer] The log severity which should be output to the log (Logger::DEBUG)
+  # @!attribute default_queue_name
+  #   @return [String] The name of the queue that will be use unless explicitly specified in a job (standard)
+  # @!attribute after_fork
+  #   @return [Proc] A proc which will be executed in a child after forking to process a message (proc {})
+  # @!attribute error_handler
+  #   @return [Proc] A proc which will be called if a job raises an exception (proc {|ex| ex })
+  # @!attribute concurrent_fork_count
+  #   @return [Integer] The number of jobs that can be processes simultanously (1)
+  #
   class Configuration
     attr_accessor :amqp, :exchange_name, :queue_name_prefix, :log_file, :log_level, :default_queue_name, :after_fork,
       :error_handler, :concurrent_fork_count
@@ -7,6 +28,8 @@ module Leveret
     def initialize
       assign_defaults
     end
+
+    private
 
     def assign_defaults
       self.amqp = "amqp://guest:guest@localhost:5672/"
@@ -16,7 +39,7 @@ module Leveret
       self.queue_name_prefix = 'leveret_queue'
       self.default_queue_name = 'standard'
       self.after_fork = proc {}
-      self.error_handler = proc {}
+      self.error_handler = proc { |ex| ex }
       self.concurrent_fork_count = 1
     end
   end
