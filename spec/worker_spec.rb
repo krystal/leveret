0183af1ac66ab2e6df920a3b5bb60fe6683ff738
Dan Wentworth <dan@atechmedia.com>
2016-08-01 10:52:42 +0100

    update worker spec to reflect new init options

--- spec/worker_spec.rb
+++ spec/worker_spec.rb
@@ -2,16 +2,6 @@ require 'spec_helper'
 
 describe Leveret::Worker do
   describe '.new' do
-    it 'can override the default worker concurrency count' do
-      current_value = Leveret.configuration.concurrent_fork_count
-      new_value = 2
-
-      _worker = Leveret::Worker.new(concurrent_fork_count: new_value)
-      expect(Leveret.configuration.concurrent_fork_count).to eq(new_value)
-
-      Leveret.configuration.concurrent_fork_count = current_value
-    end
-
     it 'uses the default queue if none is specified' do
       worker = Leveret::Worker.new
       expect(worker.queues.map(&:name)).to eq([Leveret.configuration.default_queue_name])
@@ -20,7 +10,7 @@ describe Leveret::Worker do
     it 'can use custom queue names' do
       queue_names = %w(test other)
 
-      worker = Leveret::Worker.new(queues: queue_names)
+      worker = Leveret::Worker.new(*queue_names)
       expect(worker.queues.map(&:name)).to eq(queue_names)
     end
   end
